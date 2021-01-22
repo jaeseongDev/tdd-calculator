515129554790cdfaa5a8af2d5d8684fccecc07a5
jaeseongDev <qkrwotjd1445@naver.com>
2021-01-22 14:42:31 +0900

    feat : 덧셈 기능에 대한 테스트 코드 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,10 @@
-public class CalculatorTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class CalculatorTest {
+    @Test
+    public void plus() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(calculator.plus(1, 2), 3);
+    }
 }
